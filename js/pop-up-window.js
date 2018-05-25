f86c2d41e5fffc114fa5bcfd94d9cfff63b186fe
Pansyuk <panasyuk20anna@gmail.com>
2018-05-25 17:01:37 +0300

    second part of pop-up window

--- js/pop-up-window.js
+++ js/pop-up-window.js
@@ -1,16 +1,31 @@
+var div = document.createElement('div');
+
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+
+div.style.visibility = 'hidden';
+
+document.body.appendChild(div);
+var scrollWidth = div.offsetWidth - div.clientWidth;
+document.body.removeChild(div);
+
+console.log( scrollWidth );
+
 $(function(){
     $(".btn-ct button").on('click', function() {
         if(!$(".pop-up-window").hasClass("pop-up-window-active")) {
             $(".pop-up-window").addClass("pop-up-window-active");
-            // $("html,body").addClass("body-open");
-        } else {
-            $(".pop-up-window").removeClass("pop-up-window-active");
-            // $("html,body").removeClass("body-open");
+            $("html,body").addClass("body-open");
+            $(".body-open").css("padding-right", scrollWidth);
+            console.log("dshdfh: " + scrollWidth);
         }
     });
 });
 $(function(){
     $(".pop-up-window .pop-up-toggle-ct").on('click', function() {
         $(".pop-up-window").removeClass("pop-up-window-active");
+        $("html,body").removeClass("body-open");
+        $("html,body").css("padding-right", 0);
     });
 });
