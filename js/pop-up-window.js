5da06d0a91c46f10600b33c492e4377e709b4ab6
Pansyuk <panasyuk20anna@gmail.com>
2018-05-29 13:09:06 +0300

    full pop-up window

--- js/pop-up-window.js
+++ js/pop-up-window.js
@@ -1,31 +1,39 @@
-var div = document.createElement('div');
-
-div.style.overflowY = 'scroll';
-div.style.width = '50px';
-div.style.height = '50px';
-
-div.style.visibility = 'hidden';
-
-document.body.appendChild(div);
-var scrollWidth = div.offsetWidth - div.clientWidth;
-document.body.removeChild(div);
-
-console.log( scrollWidth );
-
 $(function(){
     $(".btn-ct button").on('click', function() {
         if(!$(".pop-up-window").hasClass("pop-up-window-active")) {
             $(".pop-up-window").addClass("pop-up-window-active");
-            $("html,body").addClass("body-open");
-            $(".body-open").css("padding-right", scrollWidth);
-            console.log("dshdfh: " + scrollWidth);
+            $("body").addClass("body-scroll");
+            $(".pop-up-window").bind('mousewheel DOMMouseScroll', function(e) {
+                var scrollTo = null;
+                if (e.type == 'mousewheel') {
+                    scrollTo = (e.originalEvent.wheelDelta * -1);
+                }
+                else if (e.type == 'DOMMouseScroll') {
+                    scrollTo = 40 * e.originalEvent.detail;
+                }
+                if (scrollTo) {
+                    e.preventDefault();
+                    $(this).scrollTop(scrollTo + $(this).scrollTop());
+                }
+            });
         }
     });
 });
 $(function(){
     $(".pop-up-window .pop-up-toggle-ct").on('click', function() {
         $(".pop-up-window").removeClass("pop-up-window-active");
-        $("html,body").removeClass("body-open");
-        $("html,body").css("padding-right", 0);
+        $("body").removeClass("body-scroll");
     });
+});
+$(function() {
+   $(".pop-up-window").click(function(e) {
+       var pop_up_ct = $(".pop-up-ct");
+       var pop_up_sandwich = $(".pop-up-toggle-ct");
+       if($(".pop-up-window").hasClass("pop-up-window-active")) {
+           if ( !pop_up_ct.is(e.target) && pop_up_ct.has(e.target).length === 0 && !pop_up_sandwich.is(e.target) && pop_up_sandwich.has(e.target).length === 0 ) {
+               // $(".pop-up-window").addClass("pop-up-window-close");
+               $(".pop-up-window").removeClass("pop-up-window-active");
+           }
+       }
+   });
 });
