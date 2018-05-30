65f1c26bab2df9a3f3cb145a148cf562b546296f
Pansyuk <panasyuk20anna@gmail.com>
2018-05-30 12:01:17 +0300

    full ajax request

--- js/news.js
+++ js/news.js
@@ -7,7 +7,44 @@ $(".news-btn").on('click', function() {
             current: $(".new-ct").length
         },
         success: function(data) {
-            console.log(data);
+            var img = JSON.parse(data).body.img;
+            var title = JSON.parse(data).body.title;
+            var des = JSON.parse(data).body.des;
+            var href = JSON.parse(data).body.href;
+            var new_ct = '<div class="new-ct">' +
+                '<div class="img-ct">' +
+                '<a href="' +
+                href +
+                '">' +
+                '<img alt="img" src="' +
+                img +
+                '                   ">' +
+                '</a>' +
+                '</div>' +
+                '<div class="text-ct">' +
+                '<div class="name">' +
+                '<a href="' +
+                href +
+                '">' +
+                '<p>' +
+                title +
+                '</p>' +
+                '</a>' +
+                '</div>' +
+                '<div class="description-ct">' +
+                '<p>' +
+                des +
+                '</p>' +
+                '</div>' +
+                '<a class="read-more" href="' +
+                href +
+                '">Read More ></a>' +
+                '</div>' +
+                '</div>';
+            $(".new-ct:last").after(new_ct);
+            if($(".new-ct").length == JSON.parse(data).count_array) {
+                $(".news-btn").remove();
+            }
         }
     });
 });
--- news.php
+++ news.php
@@ -1,18 +1,25 @@
 <?php
-    echo $_POST["current"];
+    $current = $_POST["current"];
     $a = [
-                4 => [
-                    'img' => './img/news/4',
-                    'name' => 'caption 4',
-                    'des' => 'des 4',
-                    'href' => '#'
-                ],
-                5 => [
-                    'img' => './img/news/4',
-                    'name' => 'caption 5',
-                    'des' => 'des 5',
-                    'href' => '#'
-                ]
-            ];
-            echo json_encode($a);
+        1 => [],
+        2 => [],
+        3 => [],
+        4 => [
+            'img' => './img/news/4.jpg',
+            'title' => 'caption 4',
+            'des' => 'des 4',
+            'href' => '#'
+        ],
+        5 => [
+            'img' => './img/news/4.jpg',
+            'title' => 'caption 5',
+            'des' => 'des 5',
+            'href' => '#'
+        ]
+    ];
+    echo json_encode(array(
+        "body" => $a[$current + 1],
+        "count_array"=> count($a)
+    ));
+
 ?>
